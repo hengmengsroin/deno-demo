fdaa80d0c66b515e0fa7b8cb3e806515cd9db6c8
Heng Mengsroin <heng.mengsroin@optistech.com>
2022-11-18 09:57:36 +0700

    testing with oak

---
+++ oak.ts
@@ -0,0 +1,50 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+const books = new Map<string, any>();
+books.set("1", {
+  id: "1",
+  title: "The Hound of the Home",
+  author: "Conan Doyle, Arthur",
+});
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
+  })
+  .get("/book", (context) => {
+    context.response.body = Array.from(books.values());
+  })
+  .get("/book/:id", (context) => {
+    if (books.has(context?.params?.id)) {
+      context.response.body = books.get(context.params.id);
+    }
+  });
+
+const app = new Application();
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+  console.log(
+    `Listening on: ${secure ? "https://" : "http://"}${
+      hostname ?? "localhost"
+    }:${port}`
+  );
+});
+
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
