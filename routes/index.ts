c10424734f95651c5111a1b2473681bc47370edb
Heng Mengsroin <heng.mengsroin@optistech.com>
2023-02-08 09:49:44 +0700

    improve

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,27 @@
 import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import bookRouter from "./books.ts";
-import userRouter from "./users.ts";
-
-const router = new Router();
-router.all("/", (context) => {
-  context.response.body = { message: "hello from deno" };
-});
-router.all(userRouter.path, userRouter.routes);
-router.all(bookRouter.path, bookRouter.routes);
-export default router;
+import { books } from "../data.ts";
+const routes = new Router()
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
+  })
+  .post("/", (context) => {
+    context.response.body = Array.from(books.values());
+  })
+  .get("/:id", (context) => {
+    if (books.has(context?.params?.id)) {
+      context.response.body = books.get(context.params.id);
+    }
+  })
+  .patch("/:id", (context) => {
+    if (books.has(context?.params?.id)) {
+      context.response.body = books.get(context.params.id);
+    }
+  })
+  .delete("/:id", (context) => {
+    if (books.has(context?.params?.id)) {
+      context.response.body = books.get(context.params.id);
+    }
+  })
+  .routes();
+const indexRouter = { path: "/", routes };
+export default indexRouter;
